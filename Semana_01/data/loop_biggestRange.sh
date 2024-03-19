eb01328ea59d0026b9a0a9c91320a3dd1fca8d4e
Marlon Brenes <mbrenes0@hxckid.local>
2024-03-18 22:27:21 -0600

    Fix on loop_biggestRange.sh script

--- Semana_01/data/loop_biggestRange.sh
+++ Semana_01/data/loop_biggestRange.sh
@@ -2,5 +2,6 @@
 
 for dir in alexander Bert Frank_Richard
 do
-  echo "The biggest range in" ${dir} "is:" ./biggestRange.sh ${dir}
+  echo "The biggest range in" ${dir} " is:" `./biggestRange.sh ${dir}`
 done
+
