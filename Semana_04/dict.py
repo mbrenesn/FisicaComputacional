ee615904743fc51766ef90b42eb41472a0c3c72a
Marlon Brenes <brenesnm@tcd.ie>
2024-04-04 01:40:16 -0400

    Dictionary

---
+++ Semana_04/dict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Los diccionarios son mutables
+d = {'name': 'Marlon', 'age': 21}
+
+print(d['name']) # Marlon
+print(d['age']) # 21
+
+def func(dd):
+    dd['age'] = 20
+    return dd
+
+print(func(d))
