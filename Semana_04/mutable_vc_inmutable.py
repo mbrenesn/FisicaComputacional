dc4be2eec34e757ea319f72e3cac959a7639a8fd
Marlon Brenes <brenesnm@tcd.ie>
2024-04-04 01:27:48 -0400

    Mutable vs inmutable

---
+++ Semana_04/mutable_vc_inmutable.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Esto es una lista y es *mutable*
+# Es decir, si se pasa como argumento de una
+# función, la función puede modificar su contenido
+a = [3, 1, 'foo', 12.0]
+def func(l):
+    l[0] = 5
+    return l
+print(func(a)) # [5, 1, 'foo', 12.0] 
+
+# Esto es un tuple y es *inmutable*
+b = (3, 1, 'foo', 12.0)
+print(func(b)) # Runtime error. b es inmutable
