0fffaebac4745cb429ba71d3126a520cc6cfa3d2
Marlon Brenes <mbrenes0@hxckid.local>
2024-04-18 11:44:37 -0600

    Python -> Python3

--- Semana_04/dict.py
+++ Semana_04/dict.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Los diccionarios son mutables
 d = {'name': 'Marlon', 'age': 21}
--- Semana_04/mutable_vs_inmutable.py
+++ Semana_04/mutable_vs_inmutable.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Esto es una lista y es *mutable*
 # Es decir, si se pasa como argumento de una
@@ -7,7 +7,7 @@ a = [3, 1, 'foo', 12.0]
 def func(l):
     l[0] = 5
     return l
-print(func(a)) # [5, 1, 'foo', 12.0] 
+print(func(a)) # [5, 1, 'foo', 12.0]
 
 # Esto es un tuple y es *inmutable*
 b = (3, 1, 'foo', 12.0)
