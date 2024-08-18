7311fb6e50531f9988d0a4eb8f6fa4a7d1c76100
mbrenesn <marlon.brenes@utoronto.ca>
2024-08-18 13:05:18 -0600

    Deleted workspace directory for bash practice

--- Tema_01/data/biggestRange.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-grep 'Range' ${1}/* | sort -n -k 2 | tail -n 1
--- Tema_01/data/loop_biggestRange.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for dir in alexander Bert Frank_Richard
-do
-  echo "The biggest range in" ${dir} " is:" `./biggestRange.sh ${dir}`
-done
-
